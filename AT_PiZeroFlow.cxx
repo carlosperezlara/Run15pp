125ecdf55035ce62fc2e747fa5161c0944643cdf
Veronica Canoa Roman <vcanoa@rcas2066.rcf.bnl.gov>
2018-05-25 22:03:33 -0400

    pi0flow

--- AT_PiZeroFlow.cxx
+++ AT_PiZeroFlow.cxx
@@ -3,7 +3,10 @@
 #include "Analysis.h"
 #include "AT_PiZeroFlow.h"
 
-AT_PiZeroFlow::AT_PiZeroFlow() : AT_ReadTree() {
+AT_PiZeroFlow::AT_PiZeroFlow() : AT_PiZero() {
+}
+
+AT_PiZeroFlow::~AT_PiZeroFlow() {
 }
 
 void AT_PiZeroFlow::MyInit() {
@@ -13,4 +16,7 @@ void AT_PiZeroFlow::MyFinish() {
 }
 
 void AT_PiZeroFlow::MyExec() {
+  // run selector
+  AT_PiZero::MyExec();
+
 }
--- AT_PiZeroFlow.h
+++ AT_PiZeroFlow.h
@@ -1,12 +1,12 @@
 #ifndef __AT_PIZEROFLOW_HH__
 #define __AT_PIZEROFLOW_HH__
 
-#include "AT_ReadTree.h"
+#include "AT_PiZero.h"
 
-class AT_PiZeroFlow : public AT_ReadTree {
+class AT_PiZeroFlow : public AT_PiZero {
  public:
   AT_PiZeroFlow();
-  virtual ~AT_PiZeroFlow() {}
+  virtual ~AT_PiZeroFlow();
   virtual void MyInit();
   virtual void MyExec();
   virtual void MyFinish();
