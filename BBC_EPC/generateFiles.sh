ed26499de7320d36c1cad71d9f609bc9b7a0c028
carlosperezlara <cperez@rcas2066.rcf.bnl.gov>
2018-08-30 11:47:29 -0400

    update

--- BBC_EPC/generateFiles.sh
+++ BBC_EPC/generateFiles.sh
@@ -2,8 +2,9 @@
 
 for X in `cat ../runs.dat`
 do
-    #hadd out/run${X}.root out/out_${X}_*root
-    #root -b -l -q qcent.C\($X\)
+    rm out/run${X}.root
+    hadd out/run${X}.root out/out_${X}_*root
+    root -b -l -q qcent.C\($X\)
     #root -b -l -q coef.C\($X\)
-    root -b -l -q res.C\($X\)
+    #root -b -l -q res.C\($X\)
 done
--- BBC_EPC/mergePerRun.C
+++
@@ -1,5 +0,0 @@
-for X in `cat runs.bbc.dat`
-do
-rm out/out${X}.root
-hadd out/out${X}.root out/${X}_*.root
-done
